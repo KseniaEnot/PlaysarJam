8f2a20b88f8f4d669ef5bf15756f1ae9e5093f87
Alexander Sudarev <114506972+kiurus7@users.noreply.github.com>
2023-03-18 11:24:30 +0300

    рабочие Barы

--- Neon Zombies/Assets/Scripts/BarScale.cs
+++ Neon Zombies/Assets/Scripts/BarScale.cs
@@ -12,9 +12,9 @@ public class BarScale : MonoBehaviour
     public Image HappyUI;
     public Image FearUI;
     public Image SadUI;
-    public bool isSelected;
-
     public PlayerStateController StateController;
+    public float current = 0.05f;
+    public float others = 0.1f;
     public void BarScaler() {
         float width = bar.GetComponent<RectTransform>().sizeDelta.x;
         float height = bar.GetComponent<RectTransform>().sizeDelta.y;
@@ -28,21 +28,21 @@ public class BarScale : MonoBehaviour
     {
         if (StateController.PlayerState == Emotions.Fear)
         {
-            HappyUI.fillAmount += 0.1f * Time.deltaTime;
-            FearUI.fillAmount -= 0.05f * Time.deltaTime;
-            SadUI.fillAmount += 0.1f * Time.deltaTime;
+            HappyUI.fillAmount += others * Time.deltaTime;
+            FearUI.fillAmount -= current * Time.deltaTime;
+            SadUI.fillAmount += others * Time.deltaTime;
         }
         else if (StateController.PlayerState == Emotions.Happiness)
         {
-            HappyUI.fillAmount -= 0.05f * Time.deltaTime;
-            FearUI.fillAmount += 0.1f * Time.deltaTime;
-            SadUI.fillAmount += 0.1f * Time.deltaTime;
+            HappyUI.fillAmount -= current * Time.deltaTime;
+            FearUI.fillAmount += others * Time.deltaTime;
+            SadUI.fillAmount += others * Time.deltaTime;
         }
         else if(StateController.PlayerState == Emotions.Sadness)
         {
-            HappyUI.fillAmount += 0.1f * Time.deltaTime;
-            FearUI.fillAmount += 0.1f * Time.deltaTime;
-            SadUI.fillAmount -= 0.05f * Time.deltaTime;
+            HappyUI.fillAmount += others * Time.deltaTime;
+            FearUI.fillAmount += others * Time.deltaTime;
+            SadUI.fillAmount -= current * Time.deltaTime;
         }
     }
 }

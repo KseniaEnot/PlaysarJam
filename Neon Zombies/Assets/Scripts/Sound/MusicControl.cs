87159161abcadb1f3f5c16fa19bd333d218c1bac
edjackster <azarov-dima2000@yandex.ru>
2023-03-19 00:02:17 +0300

    music

--- Neon Zombies/Assets/Scripts/Sound/MusicControl.cs
+++ Neon Zombies/Assets/Scripts/Sound/MusicControl.cs
@@ -4,14 +4,38 @@ using UnityEngine;
 
 public class MusicControl : MonoBehaviour
 {
-    public static MusicControl Instanse;
-    void Awake()
-    {
+    [SerializeField]
+    private AudioSource _happyMusic;
+    [SerializeField]
+    private AudioSource _sadMusic;
+    [SerializeField]
+    private AudioSource _angryMusic;
 
-        if (Instanse == null)
+    [SerializeField]
+    private PlayerStateController _stateController;
+
+    private void Start()
+    {
+        if(_stateController != null) _stateController.StateChanged += onStateChange;
+    }
+
+    private void onStateChange(Emotions state)
+    {
+        _happyMusic.Stop();
+        _angryMusic.Stop();
+        _sadMusic.Stop();
+        switch (state)
         {
-            GetComponent<AudioSource>().Play();
-            Instanse = this;
+            case Emotions.Fear:
+                _angryMusic.Play();
+                break;
+            case Emotions.Happiness:
+                _happyMusic.Play();
+                break;
+            case Emotions.Sadness:
+                _sadMusic.Play();
+                break;
         }
     }
+
 }

24c1dd94c64209ba5aef4fd33aa9643fd2aa059b
edjackster <azarov-dima2000@yandex.ru>
2023-03-18 21:37:19 +0300

    fix paws

--- Neon Zombies/Assets/Scripts/MainMenuButtonsUI.cs
+++ Neon Zombies/Assets/Scripts/MainMenuButtonsUI.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MainMenuButtonsUI : MonoBehaviour, IEventSystemHandler
+public class MainMenuButtonsUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 	public GameObject gameObject1;
 	public void OnPointerEnter(PointerEventData eventData)
 	{
 		gameObject1.SetActive(true);
-		Debug.Log("OK");
 	}
 
 	public void OnPointerExit(PointerEventData eventData)

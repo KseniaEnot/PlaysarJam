dfac61c6b131515d0782c4d44f8502a012afaa9f
MrDoctor-666 <sheron.dragnil@mail.ru>
2023-03-18 20:50:38 +0300

    overall timer

---
+++ Neon Zombies/Assets/Scripts/GameCountdown.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameCountdown : MonoBehaviour
+{
+    [SerializeField] float overallTimeSeconds = 180f;
+    [SerializeField] Image fill;
+    [SerializeField] TextMeshProUGUI text;
+
+    private float currentTimer = 0f;
+
+    private void Start()
+    {
+        currentTimer = overallTimeSeconds;
+
+    }
+
+    private void Update()
+    {
+        currentTimer -= Time.deltaTime;
+        text.text = (Mathf.FloorToInt(currentTimer / 60)).ToString("00") + ":" + (Mathf.FloorToInt(currentTimer % 60)).ToString("00");
+        fill.fillAmount = currentTimer / overallTimeSeconds;
+
+        if (currentTimer <= 0)
+        {
+            Debug.Log("Time has ended");
+        }
+    }
+}

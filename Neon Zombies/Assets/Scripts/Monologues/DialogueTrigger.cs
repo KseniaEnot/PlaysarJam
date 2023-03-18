d56acaa2368ccbd75f69cfa7dd4710fcb2988569
MrDoctor-666 <sheron.dragnil@mail.ru>
2023-03-18 23:06:30 +0300

    dialogues

---
+++ Neon Zombies/Assets/Scripts/Monologues/DialogueTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public string fileName;
+    public bool isTriggeredByCollider = false;
+
+    private bool WasTriggered = false;
+    public void TriggerDialogue()
+    {
+        if (!WasTriggered)
+            FindObjectOfType<MonologueManager>().StartMonologue(fileName);
+        WasTriggered = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isTriggeredByCollider || WasTriggered) return;
+        TriggerDialogue();
+    }
+}
